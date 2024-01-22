dffa28459b4daac664170da526eed04c1df154c9
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-22 16:25:40 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,4 +4,5 @@ cd ~/eigenlayer/cli/operator/config/
 if [ -z operator-config.yaml ] 
 then
  cp ~/scripts/eigenlayer/config/operator-config.yaml.sample operator-config.yaml
+fi
 nano operator-config.yaml
