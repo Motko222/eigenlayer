8261ec18366f9a1466c6a1e6f1d769627976b101
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-05-05 23:01:04 +0200

    Update register.sh

--- register.sh
+++ register.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-cd ~/eigenlayer/cli/operator/config/
-eigenlayer operator register operator-config.yaml
-eigenlayer operator status operator-config.yaml
+# when installed from source
+#cd ~/eigenlayer/cli/operator/config/
+#eigenlayer operator register operator-config.yaml
+#eigenlayer operator status operator-config.yaml
+
+cd ~/eigenlayer
+./eigenlayer operator register operator.yaml
+./eigenlayer operator status operator.yaml
