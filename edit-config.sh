8c30380af960b85a4eab57edf92bd284815004fb
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-05-05 23:03:41 +0200

    Update edit-config.sh

--- edit-config.sh
+++ edit-config.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-cd ~/eigenlayer/cli/operator/config
-if [ ! -f operator-config.yaml ] 
-then
- cp ~/scripts/eigenlayer/config/operator-config.yaml.sample operator-config.yaml
-fi
-
-nano operator-config.yaml
+nano ~/eigenlayer/operator.yaml
