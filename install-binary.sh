7883dc9edb0326331274734569930f44e6c4ddc5
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-05-12 21:18:20 +0200

    Update install-binary.sh

--- install-binary.sh
+++ install-binary.sh
@@ -7,7 +7,7 @@ case $sure in
 esac
 
 if [ -f ~/eigenlayer/eigenlayer ]; then  rm ~/eigenlayer/eigenlayer; fi
-if [ ! -d ~/eigenlayer ]; then  md ~/eigenlayer; fi
+if [ ! -d ~/eigenlayer ]; then  mkdir ~/eigenlayer; fi
 
 curl -sSfL https://raw.githubusercontent.com/layr-labs/eigenlayer-cli/master/scripts/install.sh | sh -s
 mv ~/bin/eigenlayer ~/eigenlayer/eigenlayer
