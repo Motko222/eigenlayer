b8615d51cbbdaa216ddaabd075043d892d61335a
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-05-12 22:35:20 +0200

    Update create-keys.sh

--- create-keys.sh
+++ create-keys.sh
@@ -4,5 +4,3 @@ read -p "name? " name
 
 cd ~/eigenlayer
 ./eigenlayer operator keys create --key-type ecdsa $name
-./eigenlayer operator keys create --key-type bls $name
-./eigenlayer operator keys list
