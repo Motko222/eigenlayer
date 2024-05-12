ff6444eacd26b13812d12e298ba0895be5c4f0e0
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-05-12 21:17:33 +0200

    Update install-binary.sh

--- install-binary.sh
+++ install-binary.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-read -p "This action will replace old binary, are you sure? " sure
+read -p "This action will replace old binary (if it exists), are you sure? " sure
 case $sure in 
  y|Y) ;;
  *) exit 1 ;;
 esac
 
-rm ~/eigenlayer/eigenlayer
+if [ -f ~/eigenlayer/eigenlayer ]; then  rm ~/eigenlayer/eigenlayer; fi
+if [ ! -d ~/eigenlayer ]; then  md ~/eigenlayer; fi
+
 curl -sSfL https://raw.githubusercontent.com/layr-labs/eigenlayer-cli/master/scripts/install.sh | sh -s
 mv ~/bin/eigenlayer ~/eigenlayer/eigenlayer
 
